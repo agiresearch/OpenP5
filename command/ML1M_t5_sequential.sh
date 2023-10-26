761ffe30f23eb7da97823848fb8fd35a64e57bbd
shuyuan-x <47437688+shuyuan-x@users.noreply.github.com>
2023-10-26 11:12:07 -0400

    Create ML1M_t5_sequential.sh

---
+++ command/ML1M_t5_sequential.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=2,3 torchrun --nproc_per_node=2 --master_port=1234 ../src/train.py --item_indexing sequential --tasks sequential,straightforward --datasets ML1M --epochs 10 --batch_size 128 --backbone t5-small --cutoff 1024 > ../log/ML1M/ML1M_t5_sequential.log
